c615e3eef264c07172f4228bf3127eae6a7af173
lautaropastorino <lautaro.pastorino@hotmail.com>
2021-06-07 11:45:33 -0300

    Añado escena de configuración y posibilidad de navegar desde el menu y de vuelta

--- Juego/scenes/ConfigurationScene.gd
+++ Juego/scenes/ConfigurationScene.gd
@@ -2,8 +2,7 @@ extends Control
 
 
 # Declare member variables here. Examples:
-var saliendo = false # Uso esta variable para poder reproducir el SelectionSound antes de salir
-var a_config = false
+var a_menu = false
 
 
 # Called when the node enters the scene tree for the first time.
@@ -14,26 +13,20 @@ func _ready():
 func _input(event):
 	if event is InputEventKey:
 		if event.is_action_pressed("1_selection"):
-			pass # TODO: escena del juego
+			pass # TODO: Configuracion de sonido
 		elif event.is_action_pressed("2_selection"):
-			a_config = true
-			$SelectionSound.play()
+			pass # TODO: Configuracion de resolucion
+		elif event.is_action_pressed("3_selection"):
+			pass # TODO: Configuracion de accesibilidad
 		elif event.is_action_pressed("4_selection"):
-			saliendo = true
+			a_menu = true
 			$SelectionSound.play()
 
 
-func _on_SalirButton_button_up():
+func _on_VolverAlMenuButton_pressed():
+	a_menu = true
 	$SelectionSound.play()
-	saliendo = true
-
 
 
 func _on_SelectionSound_finished():
-	if saliendo: get_tree().quit()
-	elif a_config: get_tree().change_scene("res://scenes/ConfigurationScene.tscn")
-
-
-func _on_ConfigurarButton_pressed():
-	a_config = true
-	$SelectionSound.play()
+	if a_menu: get_tree().change_scene("res://scenes/MainMenu.tscn")
