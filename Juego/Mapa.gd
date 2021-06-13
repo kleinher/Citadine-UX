c6f1fd51a36ee379262647d0a871c99830589081
Hernán <hernan.k97@gmail.com>
2021-06-12 21:56:18 -0300

    Menu de selección
    agregue un menu para elegir lo que se va a arreglar

--- Juego/Mapa.gd
+++ Juego/Mapa.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
 onready var map = get_node("Mapa")
+var menu = preload("res://PopUpMenu.tscn")
+var crearMenu;
+var hayMenu = false;
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -27,25 +30,39 @@ func _process(delta):
 	var mouse_pos = get_global_mouse_position()
 	var local_position = $Mapa.to_local(mouse_pos)
 	var tile = $Mapa.world_to_map(local_position)
-	
-	if ($Mapa.get_cellv(tile) == -1) and (viejaTile != tile): 
-		$SelectionTool/indicadorMouse.position = $Mapa.map_to_world(tile)
-		$SelectionTool/indicadorMouse.modulate = Color(255,255,153)
-		$SelectionTool/indicadorMouse.modulate.a = 0.3
-		viejaTile = tile
-	elif(viejaTile != tile):
-		$SelectionTool/indicadorMouse.position = $Mapa.map_to_world(tile)
-		$SelectionTool/indicadorMouse.modulate = Color(255,0,0)
-		$SelectionTool/indicadorMouse.modulate.a = 0.3
-		viejaTile = tile
+	if !$SelectionTool/MenuContainer.get_child_count() > 0:
+		if ($Mapa.get_cellv(tile) == -1) and (viejaTile != tile):
+			#Se pone el selector amarillo 
+			$SelectionTool/indicadorMouse.position = $Mapa.map_to_world(tile)
+			$SelectionTool/indicadorMouse.modulate = Color(255,255,153)
+			$SelectionTool/indicadorMouse.modulate.a = 0.3
+			viejaTile = tile
+			#habilito el uso de menu en este tile
+			crearMenu = true
+		elif(viejaTile != tile):
+			#Se pone el selector rojo
+			$SelectionTool/indicadorMouse.position = $Mapa.map_to_world(tile)
+			$SelectionTool/indicadorMouse.modulate = Color(255,0,0)
+			$SelectionTool/indicadorMouse.modulate.a = 0.3
+			viejaTile = tile
+			crearMenu = false
 	
 func _input(event):
-	if event.is_action_pressed("mouse_lclick"):
-		var mouse_pos = get_global_mouse_position()
-		var local_position = $Mapa.to_local(mouse_pos)
-		
+	
+	var mouse_pos = get_global_mouse_position()
+	var local_position = $Mapa.to_local(mouse_pos)
+	if event.is_action_pressed("ui_accept"):
 		var tile = $Mapa.world_to_map(local_position)
 		$Posicion.text = "screen: %s \n tile: %s" % [mouse_pos,tile]
-		$Mapa.set_cell(tile.x,tile.y,1)
+		#Cuando hay un menú creado
+		if $SelectionTool/MenuContainer.get_child_count() > 0:
+			$SelectionTool/MenuContainer.get_child(0).queue_free()
+		else:
+			if crearMenu:
+				var aux = menu.instance();
+				aux.rect_position = mouse_pos
+				$SelectionTool/MenuContainer.add_child(aux)
+		
+		#$Mapa.set_cell(tile.x,tile.y,1)
 	
 	
