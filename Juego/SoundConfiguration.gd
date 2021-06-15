af85a0f8f5e9bcc614ca08e617665790cd1b2ef7
rocio.castillo <rocio.castillo@fluxit.com.ar>
2021-06-14 22:45:16 -0300

    agregando un scrip global para el sonido

---
+++ Juego/SoundConfiguration.gd
@@ -0,0 +1,53 @@
+extends Node
+var volume=0.0
+var min_vol=-24
+var music_position = 0.0
+var audio 
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	createAudioStreamPlayer()
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+#Configuracion del audio
+func createAudioStreamPlayer():
+	audio = AudioStreamPlayer.new()
+	var song = load("res://resources/Background_tune.ogg")
+	song.set_loop(true)
+	audio.stream = song
+	audio.play()
+	add_child(audio)	
+	
+func changeVolume(value):
+	print("Music changed. Level: %d" % value)
+	volume=value	
+	if _music_is_on():
+		print("Music changed. Level: %d" % volume)
+		_music("SUBIR")
+	else:
+		_music("BAJAR")
+
+func _music_is_on():
+	return volume > min_vol
+	
+func _music(action):
+	if action == "SUBIR":
+		audio.volume_db = volume
+		if !audio.is_playing():
+			audio.play(music_position)
+		print("Music changed")
+	else:
+		music_position = audio.get_playback_position()
+		audio.stop()
+		print("Music stopped")
