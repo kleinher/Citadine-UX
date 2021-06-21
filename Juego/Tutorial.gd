8333463bc4b2c189bb3a6f58bd14f59d19a389ed
Hernán <hernan.k97@gmail.com>
2021-06-21 14:25:42 -0300

    Tutorial

--- Juego/Tutorial.gd
+++ Juego/Tutorial.gd
@@ -8,7 +8,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Dialogo.popup_centered_ratio(0.5)
+	$Dialogo.popup_centered()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
