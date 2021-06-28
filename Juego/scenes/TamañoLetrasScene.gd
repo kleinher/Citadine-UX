8112c4d849f7be660db44296fda06ede8032512a
Lautaro Pastorino <49029109+lautaropastorino@users.noreply.github.com>
2021-06-28 15:10:26 -0300

    voz tamaño de letras

--- Juego/scenes/TamañoLetrasScene.gd
+++ Juego/scenes/TamañoLetrasScene.gd
@@ -8,19 +8,24 @@ var set_tamanio = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_font_sizes()
+	VoiceConfiguration.connect("voice_activated", self, "scene_voice")
+	scene_voice()
 
 func _input(event):
 	if event is InputEventKey:
 		if event.is_action_pressed("1_selection"):
 			TamanioLetrasConfig.set_tamanio_letras(2)
+			VoiceConfiguration.play_voice("res://resources/voices/FontSize/small.ogg")
 			set_tamanio = true
 			$SelectionSound.play()
 		elif event.is_action_pressed("2_selection"):
 			TamanioLetrasConfig.set_tamanio_letras(1)
+			VoiceConfiguration.play_voice("res://resources/voices/FontSize/medium.ogg")
 			set_tamanio = true
 			$SelectionSound.play()
 		elif event.is_action_pressed("3_selection"):
 			TamanioLetrasConfig.set_tamanio_letras(0)
+			VoiceConfiguration.play_voice("res://resources/voices/FontSize/big.ogg")
 			set_tamanio = true
 			$SelectionSound.play()
 		elif event.is_action_pressed("4_selection"):
@@ -40,18 +45,21 @@ func _on_SelectionSound_finished():
 
 func _on_PequeaButton_pressed():
 	TamanioLetrasConfig.set_tamanio_letras(2)
+	VoiceConfiguration.play_voice("res://resources/voices/FontSize/small.ogg")
 	set_tamanio = true
 	$SelectionSound.play()
 
 
 func _on_MedianoButton_pressed():
 	TamanioLetrasConfig.set_tamanio_letras(1)
+	VoiceConfiguration.play_voice("res://resources/voices/FontSize/medium.ogg")
 	set_tamanio = true
 	$SelectionSound.play()
 
 
 func _on_GrandeButton_pressed():
 	TamanioLetrasConfig.set_tamanio_letras(0)
+	VoiceConfiguration.play_voice("res://resources/voices/FontSize/big.ogg")
 	set_tamanio = true
 	$SelectionSound.play()
 	
@@ -61,3 +69,6 @@ func set_font_sizes():
 	$VBoxContainer/VBoxContainer/CenterContainer2/MedianoButton.get("custom_fonts/font").set_size(TamanioLetrasConfig.get_boton_size())
 	$VBoxContainer/VBoxContainer/CenterContainer3/GrandeButton
 	$IrAlMenuScene/IrAlMenuButton.get("custom_fonts/font").set_size(TamanioLetrasConfig.get_boton_size())
+
+func scene_voice():
+	$"/root/VoiceConfiguration".play_voice("res://resources/voices/FontSize/font_size_voice.ogg")
