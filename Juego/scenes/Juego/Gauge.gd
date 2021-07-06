738349aa08f130d363ef3c293df2628f9f3b47a7
Hernán <hernan.k97@gmail.com>
2021-07-05 22:48:36 -0300

    Agregado logica de dificultad
    Logica muy basica del tiempo, dentro de la barra la velocidad a la que se reduce el tiempo es inversamente proporcional al nivel y directamente proporcional a la dificultad:
    var magnitud_decrementar = (1 * Globales.config.dificultad)/(Globales.config.nivel+1)

--- Juego/scenes/Juego/Gauge.gd
+++ Juego/scenes/Juego/Gauge.gd
@@ -2,13 +2,13 @@ extends TextureProgress
 
 signal poco_tiempo
 signal tiempo_finalizado
-var magnitud_decrementar = 2
+
+var magnitud_decrementar = (1 * Globales.config.dificultad)/(Globales.config.nivel+1)
 
 func _process(delta):
 	connect("tiempo_finalizado",get_parent().get_parent().get_parent().get_parent().get_parent().get_parent(),"tiempoFinalizado")
 	if(value <= 20):
 		emit_signal("poco_tiempo")
-
 
 func _on_Tiempo_decrementar_barra_timeout():
 	value -= magnitud_decrementar
